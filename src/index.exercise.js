3691819dbe4abea4ac6e81adcd2cf02c2b353e49
RichardHpa <richard.m.hpa@gmail.com>
2021-12-13 09:27:40 +1300

    created useClient Hook

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -2,28 +2,14 @@ import {loadDevTools} from './dev-tools/load'
 import './bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import {ReactQueryConfigProvider} from 'react-query'
-import {AuthProvider} from './context/auth-context'
 import {App} from './app'
+import {AppProviders} from './context'
 
-const queryConfig = {
-  queries: {
-    useErrorBoundary: true,
-    refetchOnWindowFocus: false,
-    retry(failureCount, error) {
-      if (error.status === 404) return false
-      else if (failureCount < 2) return true
-      else return false
-    },
-  },
-}
 loadDevTools(() => {
   ReactDOM.render(
-    <ReactQueryConfigProvider config={queryConfig}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ReactQueryConfigProvider>,
+    <AppProviders>
+      <App />
+    </AppProviders>,
     document.getElementById('root'),
   )
 })
